389c9749a7d153782fdaaaa04f811c673a1f5fdd
Patrick Beltrão <patrick.beltrao@outlook.com>
2022-09-10 23:46:03 -0300

    integrando o formlário

---
+++ index.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+    <html lang="pt-br">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <!-- CSS only -->
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-iYQeCzEYFbKjA/          T2uDLTpkwGzCiq6soy8tYaI1GyVh/UjpbCx/TYkiZhlZB6+fzT" crossorigin="anonymous">
+        <title>Sistema de Cadastro</title>
+    </head>
+    <body>
+
+        <div class="container">
+            <div class="title">
+                <h1>Sistema de Cadastro</h1>
+            </div>
+            <div class="section">
+                <form action="#" method="POST">
+                    <label for="name">Name</label>
+                    <input type="text" name="name" id="name">
+                    <label for="lastName">Last Name</label>
+                    <input type="text" name="lastName" id="lastName">
+                    <label for="cpf">CPF</label>
+                    <input type="text" name="cpf" id="cpf">
+                    <label for="password">Password</label>
+                    <input type="text" name="password" id="password">
+                </form>
+                <table>
+
+                </table>
+            </div>
+        </div>
+
+
+
+
+
+
+
+
+
+
+
+
+        <!-- JavaScript Bundle with Popper -->
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-u1OknCvxWvY5kfmNBILK2hRnQC3Pr17a+RTT6rIHI7NnikvbZlHgTPOOmMi466C8" crossorigin="anonymous"></script>
+    </body>
+</html>
