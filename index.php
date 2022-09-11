e21f0af21a60e26af613b56f03724e7febd98c65
Patrick Beltrão <patrick.beltrao@outlook.com>
2022-09-11 00:04:41 -0300

    estilizando a página

--- index.php
+++ index.php
@@ -5,7 +5,8 @@
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <!-- CSS only -->
-        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-iYQeCzEYFbKjA/          T2uDLTpkwGzCiq6soy8tYaI1GyVh/UjpbCx/TYkiZhlZB6+fzT" crossorigin="anonymous">
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-iYQeCzEYFbKjA/T2uDLTpkwGzCiq6soy8tYaI1GyVh/UjpbCx/TYkiZhlZB6+fzT" crossorigin="anonymous">
+        <link rel="stylesheet" href="index.css">
         <title>Sistema de Cadastro</title>
     </head>
     <body>
@@ -15,17 +16,40 @@
                 <h1>Sistema de Cadastro</h1>
             </div>
             <div class="section">
-                <form action="#" method="POST">
-                    <label for="name">Name</label>
-                    <input type="text" name="name" id="name">
-                    <label for="lastName">Last Name</label>
-                    <input type="text" name="lastName" id="lastName">
-                    <label for="cpf">CPF</label>
-                    <input type="text" name="cpf" id="cpf">
-                    <label for="password">Password</label>
-                    <input type="text" name="password" id="password">
+                <form action="#" method="POST" class="form col-4 border p-3 bg-secondary">
+
+                    <label for="name" class="form-label">Name</label>
+                    <input type="text" name="name" id="name" class="form-control">
+
+                    <label for="lastName" class="form-label">Last Name</label>
+                    <input type="text" name="lastName" id="lastName" class="form-control"> 
+
+                    <label for="cpf" class="form-label">CPF</label>
+                    <input type="text" name="cpf" id="cpf" class="form-control">
+
+                    <label for="password" class="form-label">Password</label>
+                    <input type="password" name="password" id="password" class="form-control">
+
+                    <label for="email" class="form-label">E-mail</label>
+                    <input type="email" name="email" id="email" class="form-control">
+
                 </form>
-                <table>
+
+                <table class="table table-striped col-8">
+                    <thead>
+                        <tr>
+                            <th>
+
+                            </th>
+                        </tr>
+                    </thead>
+                    <tbody class="table-group-divider">
+                        <tr>
+                            <td>
+
+                            </td>
+                        </tr>
+                    </tbody>
 
                 </table>
             </div>
